c22d74541e0b4e80245cb7ff97eed76df5a5f0f4
Tarana-naznin <tarananaznin21@gmail.com>
2025-06-29 22:26:56 +0200

    Complete CRUD operations

--- src/main/java/com/trueact/trello/task/service/DefaultTaskService.java
+++ src/main/java/com/trueact/trello/task/service/DefaultTaskService.java
@@ -5,6 +5,7 @@ import com.trueact.trello.task.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,12 +15,27 @@ public class DefaultTaskService implements TaskService{
     private TaskRepository taskRepository;
 
     @Override
-    public void save(Task task) {
-        taskRepository.save(task);
+    public Task save(Task task) {
+        return taskRepository.save(task);
     }
 
     @Override
     public Optional<Task> findById(Long id) {
         return taskRepository.findById(id);
+    }
+
+    @Override
+    public List<Task> findAll() {
+        return taskRepository.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+    taskRepository.deleteById(id);
+    }
+
+    @Override
+    public Task update(Task task) {
+        return taskRepository.save(task);
     }
 }
--- src/main/java/com/trueact/trello/task/service/TaskService.java
+++ src/main/java/com/trueact/trello/task/service/TaskService.java
@@ -2,10 +2,17 @@ package com.trueact.trello.task.service;
 
 import com.trueact.trello.task.model.Task;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TaskService {
-    void save (Task task);
+    Task save (Task task);
 
     Optional<Task> findById(Long id);
+
+    List<Task> findAll();
+
+    void deleteById(Long id);
+
+    Task update(Task task);
 }
